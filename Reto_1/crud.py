cbcdcba101b900bfef9ca3c4251f850f96671846
jromeroc <jcamiloromeroc@gmail.com>
2020-06-22 13:43:10 -0500

    Complete exercises

--- Reto_1/crud.py
+++ Reto_1/crud.py
@@ -6,6 +6,7 @@ execute = True
 courses = []
 
 def menu():
+    """ View Menu """
     options = {
         0: 'Close',
         1: 'Create.',
@@ -17,55 +18,10 @@ def menu():
     print("*"*30)
     print("Platzi courses catalog.")
     print("*"*30)
-    print("***** Menu *****")
+    print("*" * 12 + " Menu " + "*" * 12)
 
     for i in range(len(options)):
         print('{}. {}'.format(i, options[i]))
-
-def new():
-    name = input('Insert the course name: ')
-    courses.append(name)
-    view_msn("New course {} create".format(name))
-
-def edit():
-    for i in range(len(courses)):
-            print('{}. {}'.format(i, courses[i]))
-
-    reg_edit = int(input("Select the course number do you want edit: "))
-    curso_editado = input("What is the new name for this course: ")
-    courses[reg_edit] = curso_editado
-    view_msn("New course name {}.".format(curso_editado))
-
-def delete():
-    for i in range(len(courses)):
-        print('{}. {}'.format(i, courses[i]))
-        
-    reg_delete = int(input("Select the course number do you want remove: "))
-    sure = input("Are you sure? Y/N") 
-
-    if(sure.lower() == 'y'):
-        view_msn('Course remove'.format(courses[reg_delete]))
-        courses.pop(reg_delete)
-    else:
-        view_msn('Course not removed:')
-        print (" ")
-
-
-def list_courses():
-    for i in range(len(courses)):
-        print('{}. {}'.format(i, courses[i]))
-    input('Press key for continue.')
-
-def view_msn(msn):
-    os.system('cls')
-    print(msn)
-    time.sleep(2)
-    os.system('cls')
-
-
-while execute:
-    os.system('cls')
-    menu()
 
     option = input("Select option you will use: ")
    
@@ -82,5 +38,59 @@ while execute:
         list_courses()
     else:
         view_msn("Option not exist.")
+
+def new():
+    """ Create course """
+    name = input('Insert the course name: ')
+    courses.append(name)
+    view_msn("New course {} create".format(name))
+
+def edit():
+    """ Edit course """
+    for i in range(len(courses)):
+            print('{}. {}'.format(i, courses[i]))
+
+    reg_edit = int(input("Select the course number do you want edit: "))
+    curso_editado = input("What is the new name for this course: ")
+    courses[reg_edit] = curso_editado
+    view_msn("New course name {}.".format(curso_editado))
+
+def delete():
+    """ Delete course """
+    for i in range(len(courses)):
+        print('{}. {}'.format(i, courses[i]))
+        
+    reg_delete = int(input("Select the course number do you want remove: "))
+    sure = input("Are you sure? Y/N: ") 
+
+    if(sure.lower() == 'y'):
+        view_msn('Course remove'.format(courses[reg_delete]))
+        courses.pop(reg_delete)
+    if(len(courses) == 0):
+        view_msn('Not found courses:')
+    else:
+        view_msn('Course not removed:')
+        print (" ")
+
+
+def list_courses():
+    """ View courses list """
+    for i in range(len(courses)):
+        print('{}. {}'.format(i, courses[i]))
+    input('Press key for continue.')
+
+def view_msn(msn):
+    """ View message """
+    os.system('cls')
+    print(msn)
+    time.sleep(2)
+    os.system('cls')
+
+
+while execute:
+    os.system('cls')
+    menu()
+
+    
 
     
